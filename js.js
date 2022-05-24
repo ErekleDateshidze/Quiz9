2bcca52f014e3f0272076a04bf9eab8ed7b56bb1
Erekle99 <erekledateshidze99@gmail.com>
2022-05-24 14:43:45 +0400

    1

---
+++ js.js
@@ -0,0 +1,10 @@
+var salary = prompt('ხელფასი', 0);
+
+if (salary >= 1500 && salary < 3000) {
+    alert("საშუალო ხელფასი")
+}
+else if (salary > 3000) {
+    alert("მაღალი ხელფასი")
+}
+
+
---
+++ js2.js
@@ -0,0 +1,16 @@
+
+var people = [
+    ["Tom", 25, false],
+    ["Bill", 38, true],
+    ["Alice", 23, false]
+]
+
+var first = people[0][2];
+var second = people[1][1];
+var third = people[2][2];
+
+console.log(first + " " + second + " " + third);
+
+//console.log(people[0][2]);
+//console.log(people[1][1]);
+//console.log(people[2][2]);
